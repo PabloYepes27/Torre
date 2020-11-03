83f106468e79ad278af805fc5a088d0c944743bb
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-11-02 23:32:06 -0500

    Function to find the perfect match for a job opportunity

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,6 @@
 let user_data = {}
 let job_data = {}
-
+let comparison = {}
 
 function getUserData() {
     var username = $('#input_username').val()
@@ -8,8 +8,13 @@ function getUserData() {
     
     const proxy_url = "https://cors-anywhere.herokuapp.com/"
     $.get(proxy_url + 'https://bio.torre.co/api/bios/' + username, function (data) {
-        console.log(data['strengths'])
-        user_data[username] = data
+        // console.log(data['strengths'])
+        var strengths = []
+        for (const strength in data['strengths']) {
+            const name = data['strengths'][strength]
+            strengths.push(name['name'])
+        }
+        user_data[username] = strengths
     },'json').fail(function (data) {
         console.log(data)
     })
@@ -21,19 +26,49 @@ function getPositionData() {
 
     $.get('https://torre.co/api/opportunities/' + position, function (data) {
         console.log(data)
-        job_data['objective'] = data['strengths']
+        var strengths = []
+        for (const strength in data['strengths']) {
+            const name = data['strengths'][strength]
+            strengths.push(name['name'])
+        }
+        job_data['objective'] = strengths
     },'json').fail(function (data) {
         console.log(data)
     })
 }
 
+// debug
 function watchUser() {
     console.log(user_data)
 }
 
+
 function compareUsers() {
-    for (const user in user_data.values()) {
-        console.log(user)
-        
+    for (let user in user_data) {
+        let count = 0
+        user_data[user].map((strength, index) => {
+            for (let str in job_data) {
+                job_data[str].map((stren) => {
+                    // console.log(`${stren} <==> ${strength}`)
+                    if (stren === strength) {
+                        count++
+                    }
+                })
+            }
+        })
+        comparison[user] = count
     }
+    let winner = ""
+    for (const key in comparison) {
+        let point = 0
+        if (comparison[key] > point) {
+            point = comparison[key]
+            winner = key
+        }
+    }
+    console.log(winner)
+    // console.log(Object.keys(comparison).find(key => comparison[key] === (Object.values(comparison)).rsort()[0]))
+    // // console.log(Object.values(comparison).rsort()[0])
+    // // console.log(comparison)
+
 }
